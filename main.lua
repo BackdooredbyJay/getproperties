a809b441b4cc837ce0f1744d6cce108e0b31cfac
BackdooredbyJay <joelsalinas868@icloud.com>
2025-06-01 17:58:37 -0700

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,18 @@
+local json = request({
+    Url = "https://anaminus.github.io/rbx/json/api/latest.json",
+    Method = "GET",
+}).Body
+local json_to_luatbl = function(js)
+    local converted = game:GetService("HttpService"):JSONDecode(js)
+    return converted
+end
+local function getproperties(instance)
+    local property_structure = {}
+    for i,v in pairs(json_to_luatbl(json)) do
+        if v.Class == instance.ClassName and v.type == "Property" then
+            property_structure[#property_structure+1] = v.Name
+        end
+    end
+    return property_structure
+end
+return getproperties
